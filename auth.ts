681f9d6331622249cc5ca22962003a5a10a262c9
galangjrr <galangnurfatwamaulana@gmail.com>
2026-07-24 09:33:58 +0700

    fix: add graceful exception handling and pool fallback to auth credentials endpoint

--- auth.ts
+++ auth.ts
@@ -18,31 +18,36 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       async authorize(credentials) {
         if (!credentials?.identifier || !credentials?.password) return null
 
-        const user = await db.user.findFirst({
-          where: {
-            OR: [
-              { email: credentials.identifier as string },
-              { studentProfile: { nisn: credentials.identifier as string } },
-              { teacherProfile: { nip: credentials.identifier as string } }
-            ]
+        try {
+          const user = await db.user.findFirst({
+            where: {
+              OR: [
+                { email: credentials.identifier as string },
+                { studentProfile: { nisn: credentials.identifier as string } },
+                { teacherProfile: { nip: credentials.identifier as string } }
+              ]
+            }
+          })
+
+          if (!user || !user.password) return null
+
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password as string,
+            user.password
+          )
+
+          if (!isPasswordValid) return null
+
+          return {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            role: user.role,
+            image: user.image
           }
-        })
-
-        if (!user || !user.password) return null
-
-        const isPasswordValid = await bcrypt.compare(
-          credentials.password as string,
-          user.password
-        )
-
-        if (!isPasswordValid) return null
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-          image: user.image
+        } catch (err) {
+          console.error("Auth Authorize Error:", err)
+          return null
         }
       }
     })
